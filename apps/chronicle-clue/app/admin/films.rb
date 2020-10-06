c6ed60b0ecbbc64275af96ca68dac0bab26f2936
KoheiHoshiyama <k.hoshiyama@jamcode.co.jp>
2020-10-06 15:20:52 +0900

    contentモデルのリレーション追加

--- apps/chronicle-clue/app/admin/films.rb
+++ apps/chronicle-clue/app/admin/films.rb
@@ -14,5 +14,5 @@ ActiveAdmin.register Film do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  
+
 end
--- apps/chronicle-clue/app/models/content.rb
+++ apps/chronicle-clue/app/models/content.rb
@@ -1,3 +1,10 @@
 class Content < ApplicationRecord
   belongs_to :media
+  has_one :film
+  has_many :content_countries, dependent: :destroy
+  has_many :country, through: :content_countries
+  has_many :content_languages, dependent: :destroy
+  has_many :language, through: :content_languages
+  has_many :content_categories, dependent: :destroy
+  has_many :category, through: :content_categories
 end
